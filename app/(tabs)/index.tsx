7c33db83524b0325f0b70f006e5c0a8cf1ac4824
Kyle Lin <linkyle0924@gmail.com>
2025-05-07 22:39:48 -0700

    Add background color

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,35 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
 
 
 export default function HomeScreen() {
   return (
     <View style={styles.mainText}>
-      <Text>
-        Home screen
-      </Text>
+      <LinearGradient
+        colors = {['rgb(91, 73, 173)', 'rgb(45, 36, 87)']}
+        style = {styles.gradient}
+      >
+        <Text style = {styles.homePageText}>
+          Home screen
+        </Text>
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   mainText:{
+    flex: 1
+  },
+
+  gradient:{
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+    flex: 1,
+  },
 
-  }
+  homePageText: {
+    color: 'white'
+  },
 
 });
