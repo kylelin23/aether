549ea29fcd2f3df639567f786f122cb75e0a638a
Kyle Lin <linkyle0924@gmail.com>
2025-05-22 17:00:31 -0700

    Add Sign Up Button

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,11 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-import HomeScreen from './(tabs)';
-import Navigator from './routes/startingStack';
+import Navigator from './routes/logInStack';
+
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
