e4cd44a69400b10d7611a30fc7f8e7b35835e4a7
Kyle Lin <linkyle0924@gmail.com>
2025-06-24 12:06:22 +0800

    Add stats screen

---
+++ app/(tabs)/statsScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function StatsScreen(){
+
+    return(
+        <View>
+            <Text>Stats Screen</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+});
