0682999e238f635336c85ee429e02b8f174ede65
Kyle Lin <linkyle0924@gmail.com>
2025-06-24 13:25:51 +0800

    Add Overview and Categories

--- app/(tabs)/statsScreen.tsx
+++ app/(tabs)/statsScreen.tsx
@@ -1,14 +1,23 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet, Dimensions } from 'react-native';
 
 export default function StatsScreen(){
 
     return(
         <View>
-            <Text>Stats Screen</Text>
+            <View style = {styles.container}>
+                <Text>Overview</Text>
+                <Text>Categories</Text>
+            </View>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
+const screenWidth = Dimensions.get('window').width;
 
+const styles = StyleSheet.create({
+    container: {
+      flexDirection: 'row',
+      width: screenWidth,
+      justifyContent: 'space-around'
+    }
 });
