2c45990b985ce58e38c703778840bb4d88d2d977
Kyle Lin <linkyle0924@gmail.com>
2025-07-20 19:18:12 -0700

    Add tab

--- app/(tabs)/statsScreen.tsx
+++ app/(tabs)/statsScreen.tsx
@@ -1,23 +1,35 @@
-import { Text, View, StyleSheet, Dimensions } from 'react-native';
+import { Text, View, StyleSheet, Dimensions, Button } from "react-native";
+import OverviewScreen from "./overview";
+import CategoriesScreen from "./categories";
+import { useState } from "react";
 
-export default function StatsScreen(){
+export default function StatsScreen() {
+  const [selectedTab, setSelectedTab] = useState<"overview" | "categories">(
+    "overview"
+  );
 
-    return(
-        <View>
-            <View style = {styles.container}>
-                <Text>Overview</Text>
-                <Text>Categories</Text>
-            </View>
-        </View>
-    )
+  return (
+    <View>
+      <View style={styles.container}>
+        <Button title="Overview" onPress={() => setSelectedTab("overview")} />
+        <Button
+          title="Categories"
+          onPress={() => setSelectedTab("categories")}
+        />
+      </View>
+      <View>
+        {selectedTab === "overview" ? <OverviewScreen /> : <CategoriesScreen />}
+      </View>
+    </View>
+  );
 }
 
-const screenWidth = Dimensions.get('window').width;
+const screenWidth = Dimensions.get("window").width;
 
 const styles = StyleSheet.create({
-    container: {
-      flexDirection: 'row',
-      width: screenWidth,
-      justifyContent: 'space-around'
-    }
+  container: {
+    flexDirection: "row",
+    width: screenWidth,
+    justifyContent: "space-around",
+  },
 });
