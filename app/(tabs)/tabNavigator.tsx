9698bac28cabe710cafb2c7dce1819e323896bae
Kyle Lin <linkyle0924@gmail.com>
2025-08-30 17:17:57 -0700

    Add tab bar icons

--- app/(tabs)/tabNavigator.tsx
+++ app/(tabs)/tabNavigator.tsx
@@ -4,20 +4,37 @@ import SettingsScreen from "./settings";
 import WalletScreen from "./wallet";
 import GameScreen from "./game";
 import ProfileScreen from "./profile";
+import { Ionicons } from '@expo/vector-icons';
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
   return (
     <Tab.Navigator initialRouteName="Stats"
-    screenOptions = {{
+    screenOptions = { ({route}) => ({
+        tabBarIcon: ({ color, size }) => {
+            let iconName;
+            if (route.name === "Wallet") {
+              iconName = "wallet-outline";
+            } else if (route.name === "Profile") {
+              iconName = "person-outline";
+            } else if (route.name === "Stats") {
+              iconName = "stats-chart-outline";
+            } else if (route.name === "Game") {
+              iconName = "game-controller-outline";
+            } else if (route.name === "Settings") {
+              iconName = "settings-outline";
+            }
+
+            return <Ionicons name={iconName as keyof typeof Ionicons.glyphMap} size={size} color={color} />;
+          },
         tabBarStyle: {
             backgroundColor: 'black',
         },
         tabBarActiveTintColor: 'rgb(91, 73, 173)',
         tabBarInactiveTintColor: 'white',
         headerShown: false,
-    }}
+    })}
     >
       <Tab.Screen name="Wallet" component={WalletScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
